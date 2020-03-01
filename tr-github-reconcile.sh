ea310c974c57d71603905052a1d62746b862d98e
actuated <actuated@users.noreply.github.com>
2020-03-01 11:38:51 -0600

    Added wgexifloop to repo list

--- tr-github-reconcile.sh
+++ tr-github-reconcile.sh
@@ -17,6 +17,7 @@
 # 10/12/2018 - Added -i option to limit repo list to tools useful for internal pentests
 # 1/8/2019 - Added nse-parse
 # 2/29/2020 - Added sleepscan
+# 3/1/2020 - Added wgexifloop
 
 # Variable for the default root directory that contains downloaded tools.
 # Change this to your own existing or desired directory.
@@ -30,7 +31,7 @@ varRootDir="/tr/github/"
 # 1. Change the variable below to use your github account URL (include the trailing /):
 varMyGitUrlRoot="https://github.com/actuated/"
 # 2. List your repos in this variable, which fnCheckRepos will use in a for loop.
-varRepoList="brute-probe check-smb-signing cisco-config-check find-reused-ntds-hashes ike-trans login-loop msf-exploit-loop nmap-grep nmap-script-parsers nse-parse pass-survey range-finder rogue-robin runas_system sleepscan SiteSurvey smb-anon-shares sslscanalyzer soc-eng-batches teradata-login tr-github-reconcile user-enum-demo-pages user-parse"
+varRepoList="brute-probe check-smb-signing cisco-config-check find-reused-ntds-hashes ike-trans login-loop msf-exploit-loop nmap-grep nmap-script-parsers nse-parse pass-survey range-finder rogue-robin runas_system sleepscan SiteSurvey smb-anon-shares sslscanalyzer soc-eng-batches teradata-login tr-github-reconcile user-enum-demo-pages user-parse wgexifloop"
 # To specify repo list when -i option is used, search for the comment "Check for options."
 
 
